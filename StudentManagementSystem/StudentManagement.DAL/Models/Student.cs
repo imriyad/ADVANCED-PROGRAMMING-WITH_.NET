0cca57b6699ab5a2a71723bb35213d73056019b5
Mahdi Kabir Riyad <114643825+imriyad@users.noreply.github.com>
2025-06-14 22:56:16 +0600

    .

---
+++ StudentManagementSystem/StudentManagement.DAL/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentManagement.DAL.Models
+{
+    [Table("Students")]
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string FullName { get; set; }
+
+        public int Age { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
