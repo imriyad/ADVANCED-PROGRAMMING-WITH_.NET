87c10689b2268999e3383f88ba1e59f7a79e4c8e
Mahdi Kabir Riyad <114643825+imriyad@users.noreply.github.com>
2025-06-15 18:56:11 +0600

    .

---
+++ StudentManagementSystem/StudentManagement.Web/Controllers/StudentController.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Mvc;
+using StudentManagement.BLL;
+using StudentManagement.DAL.Models;
+
+namespace StudentManagement.Web.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly StudentService _service = new StudentService();
+
+        public ActionResult Index()
+        {
+            var students = _service.GetAllStudents();
+            return View(students);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                _service.AddStudent(student);
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+    }
+}
---
+++ StudentManagementSystem/StudentManagement.Web/Controllers/StudentService.cs
@@ -0,0 +1,18 @@
+﻿using StudentManagement.DAL.Models;
+using System;
+
+namespace StudentManagement.Web.Controllers
+{
+    internal class StudentService
+    {
+        internal object GetAllStudents()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void AddStudent(Student student)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
